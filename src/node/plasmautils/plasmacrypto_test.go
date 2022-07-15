1184957a56e908fe46aad9c892954d2b187404ec
kekeshiM0chi <frogandfrog009@gmail.com>
2022-07-15 13:27:53 +0900

    feat: add the test on working

--- src/node/plasmautils/plasmacrypto_test.go
+++ src/node/plasmautils/plasmacrypto_test.go
@@ -1,18 +1,28 @@
 package test
 
 import (
+	"fmt"
+	"github.com/BANKEX/plasma-research/src/node/plasmautils/plasmacrypto"
+	"github.com/stretchr/testify/assert"
+	"math/big"
 	"testing"
 )
 
 func TestRSAAccumulator(t *testing.T) {
 	// TODO: test package not working
-	// a := new(plasmacrypto.Accumulator).SetInt(big.NewInt(17))
-	// a.Accumulate(19)
-	// res, _ := new(big.Int).SetString("239072435685151324847153", 10)
-	// ttl.Assert(t, a.Value().Cmp(res) == 0, "Wrong accumulator value")
-	//
-	// a = new(plasmacrypto.Accumulator).SetInt(big.NewInt(17))
-	// a.BatchAccumulate([]uint32{3, 5})
-	// res, _ = new(big.Int).SetString("2862423051509815793", 10)
-	// ttl.Assert(t, a.Value().Cmp(res) == 0, "Wrong accumulator value")
+
+	a := new(plasmacrypto.Accumulator).SetInt(big.NewInt(17))
+	a.Accumulate(19)
+	res, _ := new(big.Int).SetString("239072435685151324847153", 10)
+	fmt.Println("", res)
+	a.Value().Cmp(res)
+	fmt.Println("", res)
+
+	assert.Equal(t, a.Value(), res, "Right accumulator value")
+
+	a = new(plasmacrypto.Accumulator).SetInt(big.NewInt(17))
+	a.BatchAccumulate([]uint32{3, 5})
+	res, _ = new(big.Int).SetString("2862423051509815793", 10)
+	fmt.Println("", res)
+	assert.Equal(t, a.Value(), res, "Right accumulator value")
 }
